e4f2b46076cb1fd11304b6e1402f4e62fd1e15b6
Mary Gualtieri <mgualtieri7@gmail.com>
2016-01-04 10:30:43 -0500

    added address key value

--- Contact_manager.js
+++ Contact_manager.js
@@ -2,11 +2,13 @@ var friends = {
     bill: {
         firstName: "Bill" ,
         lastName: "Bob" ,
-        number: 234-543-3456
+        number: "234-543-3456",
+        address: ['1234 Malberry Lane, Davie, FL 321234']
         },
     steve: {
         firstName: "Steve",
         lastName: "Joe",
-        number: 313-232-4567
+        number: "313-232-4567",
+        address: ["825 Holly Lane, Plantation, FL 33317"]
         }
     }
